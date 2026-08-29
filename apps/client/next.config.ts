18a31f58db60c80bf8756c534fedd8ae24c86d34
Malalang <admin@malalang.co.za>
2026-08-29 16:48:14 +0200

    transpile shared constants package in Next client

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@kings-of-cars/constants'],
   images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
 }
 
